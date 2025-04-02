9729e9e1a7d1868092dab443db5b9b77736571b2
Duong Quoc Nghi <duongquocnghi2016@gmail.com>
2025-04-02 21:52:33 +0700

    cập nhật giao diện WeatherLoading

---
+++ lib/core/widgets/rotating_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class RotatingWidget extends StatefulWidget {
+  final Widget child;
+
+  const RotatingWidget({super.key, required this.child});
+
+  @override
+  _RotatingWidgetState createState() => _RotatingWidgetState();
+}
+
+class _RotatingWidgetState extends State<RotatingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _controller.value * 2 * 3.14159265359,
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
--- lib/features/weather/widgets/weather_loading.dart
+++ lib/features/weather/widgets/weather_loading.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/src/generated/i18n/app_localizations.dart';
+import 'package:weather_app/core/widgets/rotating_widget.dart';
 
 class WeatherLoading extends StatelessWidget {
-  const WeatherLoading({Key? key}) : super(key: key);
+  const WeatherLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          ),
-          const SizedBox(height: 16),
-          Text(
-            AppLocalizations.of(context)?.loading_weather ??
-                'Đang tải dữ liệu thời tiết...',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ],
+      child: RotatingWidget(
+        child: Icon(Icons.autorenew_rounded, size: 96,),
       ),
     );
   }
